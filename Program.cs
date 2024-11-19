bc4745c550ec857d85a2d99049327b8207d9a93c
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-19 09:54:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignments_3._1._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 0, 2, 1, 1, 9, 1, 1 };
+            Console.WriteLine("Original Array: [" + string.Join(", ", inputArray) + "]");
+
+            ModifyArray(inputArray);
+
+            Console.WriteLine("Modified Array: [" + string.Join(", ", inputArray) + "]");
+        }
+
+        static void ModifyArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == 1 && arr[i + 1] == 1) 
+                {
+                    arr[i] = 0;
+                    arr[i + 1] = 0;
+                    break; 
+                }
+            }
+        }
+    }
+}
